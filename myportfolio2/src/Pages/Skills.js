1bc33fad78474cc4d33a0576216e0bbd0a31245b
Christopher Roldán Sánchez <Christopher_rol@hotmail.es>
2021-08-26 00:02:54 -0300

    add skills page

---
+++ myportfolio2/src/Pages/Skills.js
@@ -0,0 +1,27 @@
+import Title from "../Components/Title";
+import React, { useContext } from "react";
+import LanguageContext from "../Context/LangContext";
+import { tecno, tools } from "../API/works.json";
+import SkillCard from "../Components/SkillList";
+import styled from "styled-components";
+const SkillContainer = styled.article`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: space-around;
+`;
+
+function Skills() {
+  const { text } = useContext(LanguageContext);
+  return (
+    <div>
+      <Title>{text.skills}</Title>
+      <SkillContainer>
+        <SkillCard type="Lenguages" value={tecno} />
+        <SkillCard type="Tools" value={tools} />
+      </SkillContainer>
+    </div>
+  );
+}
+
+export default Skills;
